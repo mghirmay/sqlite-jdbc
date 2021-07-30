cd43ca6f0784994b2f06cae7183e85abef6847a9
mghirmay <musie.ghirmay@gmail.com>
2021-07-30 22:19:34 +0200

    add a module-info file and updated to latest plugin version

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module org.sqlite.sqlite {
+    requires java.logging;
+    requires java.sql;
+    //requires org.slf4j;  //deleted this
+  
+    exports org.sqlite;
+    exports org.sqlite.core;
+    exports org.sqlite.date;
+    exports org.sqlite.javax;
+    exports org.sqlite.jdbc3;
+    exports org.sqlite.jdbc4;
+    exports org.sqlite.util;
+
+}
